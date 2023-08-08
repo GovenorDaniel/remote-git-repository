d62a7223ecd4916b18ae0a70a0003f01ab984345
Daniel Borke <dborke36@gmail.com>
2023-08-08 20:20:06 +0000

    updated

---
+++ javascript/javascript/java.js
@@ -0,0 +1,10 @@
+
+
+
+let pens =10;
+let books =20;
+
+
+
+console.log(pens--)
+console.log(pens)
---
+++ javascript/javascript/myscript.js
@@ -0,0 +1,11 @@
+
+console.log("my link is connected")
+let pens =10;
+let books =20;
+
+
+
+console.log(pens--)
+console.log(pens)
+
+document.write('this is me')
